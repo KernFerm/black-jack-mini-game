d75136705ea448ca59e42a84ad85348cd537f269
Bubbles The Dev <152947339+KernFerm@users.noreply.github.com>
2024-10-07 07:32:36 -0400

    Update install.bat
    Signed-off-by: Bubbles The Dev <152947339+KernFerm@users.noreply.github.com>

--- install.bat
+++ install.bat
@@ -1,26 +1,10 @@
 @echo off
-:: Batch script to install colorama, setuptools, and tkinter with a loading bar for finalization
+:: Install required packages
 
-echo Installing setuptools...
-pip install setuptools>=75.1.0
+pip install setuptools>=75.1.0 colorama>=0.4.5 tk
 
-echo Press Enter to continue to install the other dependencies.
-pause
-
-echo Installing colorama...
-pip install colorama>=0.4.5
-
-echo Press Enter to continue to install tkinter.
-pause
-
-echo Installing tkinter...
-pip install tk
-
-echo Press Enter to finalize the install.
-pause
-
-:: Finalization with a loading bar
-echo Finalizing...
+:: Loading bar
+echo Finalizing installation...
 setlocal
 set "progressBar="
 for /L %%A in (1,1,10) do (
@@ -30,6 +14,5 @@ for /L %%A in (1,1,10) do (
 )
 
 endlocal
-echo All packages have been installed!
-echo You may now exit the cmd.exe.
+echo Installation complete!
 pause
